2dcaa469918dab8a2f553958e0a3311b5ccba333
Heesang <ghs050611@naver.com>
2024-09-10 21:05:44 +0900

    Add Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
 export default function Header() {
     return (
-        <div>
-            <img src="/images/logo.svg"/>
+        <div className="flex justify-between items-center py-10 px-24">
+            <img src="/images/logo.svg" className="w-52"/>
+            <nav className="text-white text-lg font-bold flex space-x-10">
+                <p>박스오피스</p>
+                <p>검색</p>
+            </nav>
         </div>
     )
 }
