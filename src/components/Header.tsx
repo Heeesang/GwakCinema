3de70c68d0d619778f74e145dc3dc33d39c9f791
Heesang <ghs050611@naver.com>
2024-09-11 02:15:02 +0900

    Update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
-        <div className="flex justify-between items-center py-10 px-24">
+        <div className="flex justify-between items-center py-10 px-28">
             <img src="/images/logo.svg" className="w-52"/>
-            <nav className="text-white text-lg font-bold flex space-x-10">
+            <nav className="text-[#B6B6B6] text-xl font-bold flex space-x-10">
                 <p>박스오피스</p>
                 <p>검색</p>
             </nav>
