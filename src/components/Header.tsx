26506f2781c4972ab22776cb9cc1c84aede288ec
Heesang <ghs050611@naver.com>
2024-09-13 19:53:28 +0900

    Update main

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <div className="flex justify-between items-center py-10">
-            <img src="/images/logo.svg" className="w-52"/>
+            <img src="/images/logo.svg" className="w-40"/>
             <nav className="text-[#B6B6B6] text-xl font-bold flex space-x-10">
                 <p>박스오피스</p>
                 <p>검색</p>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -20,8 +20,15 @@ export default function Main() {
     }, []);
     
     return (
-        <div className="bg-black px-40 h-dvh">
+        <div className="bg-black px-40">
             <Header/>
+            <div className="flex flex-col justify-center items-center my-40">
+                <img src="/images/logo.svg" className="w-5/12 my-8"/>
+                <input 
+                    className="w-4/12 h-12 bg-black border-gray-200 border rounded-lg placeholder: font-semibold pl-3"
+                    placeholder="영화제목 검색"    
+                />
+            </div>
             <h1 className="text-white mt-10 mb-8 text-4xl font-bold">박스오피스</h1>
             <div className="flex justify-between pb-52">
             <Movie movie={movies[0]}/>
