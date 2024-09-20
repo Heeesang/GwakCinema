0c1fae4f1a6f1d3a9e3502b2fae36ee60b71b1c4
Heesang <ghs050611@naver.com>
2024-09-20 17:52:21 +0900

    Update Movie categoryButton

--- src/components/MovieCategoryButton.tsx
+++ src/components/MovieCategoryButton.tsx
@@ -3,7 +3,8 @@ export default function MovieCategoryButton() {
         <div className="mx-auto max-w-screen-2xl">
             <img src='/images/koreamovies.svg' className="w-full"/>
             <div className="flex items-center absolute inset-0 bg-gradient-to-r from-black to-transparent">
-                <h1 className="text-6xl text-gray-400 font-bold pl-32">국내영화</h1>
+                <h1 className="text-6xl text-[#B6B6B6] font-bold pl-32">국내영화</h1>
+                <img src="/images/arrow.svg" className="w-6 ml-10"/>
             </div>
         </div>
     )
