8e9066eae7a7be7baa1df2139d4435f0658b837f
Heesang <ghs050611@naver.com>
2024-09-22 14:53:30 +0900

    Update categorybutton padding

--- src/components/MovieCategoryButton.tsx
+++ src/components/MovieCategoryButton.tsx
@@ -3,7 +3,7 @@ export default function MovieCategoryButton() {
         <div>
             <img src='/images/koreamovies.svg' className="w-full"/>
             <div className="flex items-center absolute inset-0 bg-gradient-to-r from-black to-transparent">
-                <p className="text-6xl text-[#B6B6B6] font-bold pl-32">국내영화</p>
+                <p className="text-6xl text-[#B6B6B6] font-bold pl-10">국내영화</p>
                 <img src="/images/arrow.svg" className="w-6 ml-10"/>
             </div>
         </div>
