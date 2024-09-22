4b326eec0aa1ebfb06364d4e781d1f5b8e72226c
Heesang <ghs050611@naver.com>
2024-09-22 15:34:12 +0900

    Update categoryButton hover

--- src/components/MovieCategoryButton.tsx
+++ src/components/MovieCategoryButton.tsx
@@ -1,8 +1,8 @@
 export default function MovieCategoryButton() {
     return (
-        <div>
-            <img src='/images/koreamovies.svg' className="w-full"/>
-            <div className="flex items-center absolute inset-0 bg-gradient-to-r from-black to-transparent">
+        <div className="relative overflow-hidden">
+            <img src='/images/koreamovies.svg' className="w-full transform hover:scale-105 transition-transform duration-300"/>
+            <div className="flex items-center absolute inset-0 bg-gradient-to-r from-black to-transparent pointer-events-none">
                 <p className="text-6xl text-[#B6B6B6] font-bold pl-10">국내영화</p>
                 <img src="/images/arrow.svg" className="w-6 ml-10"/>
             </div>
