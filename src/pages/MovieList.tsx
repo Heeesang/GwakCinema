e87c96954169e028125ea29be5c9dd1d688a39de
Heesang <ghs050611@naver.com>
2024-10-11 17:15:52 +0900

    Update movieList

--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -9,7 +9,7 @@ export default function MovieList() {
         <div className="bg-black">
             <Header/>
             <div className=" max-w-screen-2xl mx-auto px-40 pt-32">
-                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-between gap-6">
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {movieList?.slice(0, 15).map((movie, index) => (
                         <Movie key={index} movie={movie} index={index} showIndex={false} />
                     ))}
