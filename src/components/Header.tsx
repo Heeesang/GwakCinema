b569e2db6764ff6e48ca273f6e176f496ad248cc
Heesang <ghs050611@naver.com>
2024-10-11 16:07:39 +0900

    Update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <div className="absolute left-1/2 -translate-x-1/2 mx-auto max-w-screen-2xl flex justify-between items-center py-10 px-40 top-0 w-full z-10">
+        <div className="fixed backdrop-blur-sm left-1/2 -translate-x-1/2 mx-auto max-w-screen-2xl flex justify-between items-center py-10 px-40 top-0 w-full z-10">
             <img src="/images/logo.svg" className="w-40"/>
             <nav className="text-[#B6B6B6] text-xl font-bold flex space-x-10">
                 <p>박스오피스</p>
