5b1f994098e70bc631fdb6a30d137183dfa9e9b7
Heesang <ghs050611@naver.com>
2024-10-11 16:48:07 +0900

    Add movieListPage

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -3,17 +3,21 @@ import { MovieType } from "../types/MovieType";
 interface MovieProps {
     movie: MovieType;
     index: number;
-  }
+}
 
-export default function Movie({movie, index}: MovieProps) {
+export default function Movie({ movie, index, showIndex = true }: MovieProps & { showIndex?: boolean }) {
     return (
-        <div className="bg-gray-300 rounded-xl h-80 w-56 hover:scale-110 transform transition duration-300 flex items-center justify-center overflow-hidden">
+        <div className="bg-gray-300 rounded-xl h-full w-full hover:scale-110 transform transition duration-300 flex items-center justify-center overflow-hidden">
             <img
                 src={movie?.posters?.split('|')[0]}
                 alt="Movie Poster"
                 className="object-cover w-full h-full"
             />
-            <p className="absolute left-0 bottom-0 m-3 text-black text-8xl font-bold text-stroke">{index + 1}</p>
+            {showIndex && (
+                <p className="absolute left-0 bottom-0 m-3 text-black text-8xl font-bold text-stroke">
+                    {index + 1}
+                </p>
+            )}
         </div>
     )
 }
--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -1,6 +1,20 @@
+import Header from "../components/Header";
+import Movie from "../components/Movie";
+import { useGetMovieList } from "../hooks/useGetMovieList"
+
 export default function MovieList() {
+    const { data: movieList, isLoading } = useGetMovieList();
+
     return (
         <div className="bg-black">
+            <Header/>
+            <div className=" max-w-screen-2xl mx-auto px-40 pt-32">
+                <div className="grid grid-cols-4 justify-between gap-6">
+                    {movieList?.slice(0, 15).map((movie, index) => (
+                        <Movie key={index} movie={movie} index={index} showIndex={false} />
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
