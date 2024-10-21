3f80099c1caf30fecd76d0d7c56dd2f5f84c1d75
Heesang <ghs050611@naver.com>
2024-10-21 17:53:56 +0900

    Add movieDetail movie plot

--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -7,24 +7,33 @@ import { useState } from "react";
 
 export default function MovieDetail() {
     const { title, movieSeq } = useParams();
-    const { data } = useGetMovieDetail(title || "", movieSeq || "");
+    const decodedTitle = decodeURIComponent(title || "");
+    const { data } = useGetMovieDetail(decodedTitle, movieSeq || "");
 
     const directorName = data?.director
     const plotText = data?.plots
     const posterSrc = data?.posters
     const stills = data?.stlls
+    const actors = data?.actors
 
     return (
-        <div className="text-white px-40 max-w-screen-2xl mx-auto relative">
-            <Header/>
-            <div className="absolute inset-0 h-96 w-ful">
-                <img src={stills} className="object-cover h-full w-full blur-sm opacity-50"/>
-            </div>
-            <div className="flex items-center ml-10 pt-60">
-                <img src={posterSrc} className=""/>
-                <div className="ml-10">
-                    <h1 className="text-5xl font-bold">{title}</h1>
-                    <h2 className="text-lg font-semibold mt-5">감독: {directorName}</h2>
+        <div>
+            <Header />
+            <div className="text-white px-40 max-w-screen-2xl mx-auto relative">
+                <div className="absolute inset-0 h-96 w-ful">
+                    <img src={stills} className="object-cover h-full w-full blur-sm opacity-50" />
+                </div>
+                <div className="flex items-center pt-60">
+                    <img src={posterSrc} className="" />
+                    <div className="ml-10">
+                        <h1 className="text-5xl font-bold">{title}</h1>
+                        <h2 className="text-lg font-semibold mt-5">감독: {directorName}</h2>
+                        <h2 className="text-lg font-semibold">배우: {actors}</h2>
+                    </div>
+                </div>
+                <div className="mt-20">
+                    <h1 className="font-bold text-3xl">줄거리</h1>
+                    <p className="font-medium text-2xl mt-5">{plotText}</p>
                 </div>
             </div>
         </div>
