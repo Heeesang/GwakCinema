8bfb7d04b225516683fbcb46af7980da585ce11f
Heesang <ghs050611@naver.com>
2024-10-22 18:03:26 +0900

    Update environment

--- src/api/MovieList.tsx
+++ src/api/MovieList.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { MovieType, MovieResponse, BoxOfficeResponse } from "../types/MovieType";
+import { MovieType, MovieResponse, BoxOfficeResponse, MovieDetailType, MovieDetailResponse } from "../types/MovieType";
 
 const formatTitle = (title: string) => {
   return title
@@ -13,26 +13,59 @@ const formatTitle = (title: string) => {
 let searchCallCount = 0;
 
 const baseURL = process.env.REACT_APP_MOVIELIST_URL
+const movieListKey = process.env.REACT_APP_MOVIELIST_KEY
+const boxOfficeURL = process.env.REACT_APP_BOXOFFICE_URL
+const boxOfficeKey = process.env.REACT_APP_BOXOFFICE_KEY
 
 export const fetchMovies = async () => {
   try {
-    console.log(baseURL)
-    const response = await axios.get<MovieResponse>(`${baseURL}&ServiceKey=923AT6VA7NPY9SRIQ6T2&releaseDts=20240801&listCount=300`);
-    console.log(response.data.Data[0].Result)
-    return response.data.Data[0].Result;
+    const response = await axios.get<MovieResponse>(`${baseURL}&ServiceKey=${movieListKey}&releaseDts=20240801&listCount=300`);
+
+    // 필요한 정보만 추출해서 반환
+    const movies = response.data.Data[0].Result.map((movie) => ({
+      movieSeq: movie.movieSeq,
+      title: movie.title,          // 영화 제목
+      posters: movie.posters?.split('|')[0] || '',  // 포스터 이미지 (첫 번째 이미지)
+    }));
+
+    console.log(response.data.Data[0]);  // 필터된 영화 목록 확인
+    return movies;
   } catch (error) {
     console.error('API 요청 에러:', error);
     throw error;
   }
 };
 
+export const fetchMovieDetail = async (movieNm: string, movieSeq: string) => {
+  console.log(movieNm)
+  try {
+    // 필요한 경우 추가 API 호출 또는 별도 검색 로직
+    const searchResponse = await axios.get<MovieDetailResponse>(`${baseURL}&ServiceKey=${movieListKey}&title=${movieNm}&movieSeq=${movieSeq}`);
+    console.log("hi")
+    console.log(searchResponse.data.Data[0].Result[0])
+
+    const detailData = {
+      posters: searchResponse.data.Data[0].Result[0].posters.split('|')[0],
+      plots: searchResponse.data.Data[0].Result[0].plots.plot[0].plotText,
+      director: searchResponse.data.Data[0].Result[0].directors.director[0].directorNm,
+      stlls: searchResponse.data.Data[0].Result[0].stlls.split('|')[0],
+      actors: searchResponse.data.Data[0].Result[0].actors.actor[0].actorNm
+    };
+
+    return detailData
+  } catch (error) {
+    console.error('검색 요청 에러:', error);
+    throw error; // 실패 시 포스터 없음 처리
+  }
+};
+
 // 영화 이름으로 검색하는 비동기 함수 (필요할 경우 API 추가 호출)
-const searchMovieByName = async (movieNm: string) => {
+const searchMoviePoster = async (movieNm: string) => {
   searchCallCount++;
   console.log(movieNm)
   try {
     // 필요한 경우 추가 API 호출 또는 별도 검색 로직
-    const searchResponse = await axios.get<MovieResponse>(`https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=923AT6VA7NPY9SRIQ6T2&title=${movieNm}`);
+    const searchResponse = await axios.get<MovieResponse>(`${baseURL}&ServiceKey=${movieListKey}&title=${movieNm}`);
     return searchResponse.data.Data[0].Result[0]?.posters || '포스터 없음';
   } catch (error) {
     console.error('검색 요청 에러:', error);
@@ -43,9 +76,9 @@ const searchMovieByName = async (movieNm: string) => {
 export const fetchBoxOffice = async (movieList: MovieType[]) => {
   try {
     // 박스오피스 영화 목록 가져오기
-    const boxOfficeResponse = await axios.get<BoxOfficeResponse>('https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=c9777b972b29ca8836e49e0abd905799&targetDt=20240925');
+    const boxOfficeResponse = await axios.get<BoxOfficeResponse>(`${boxOfficeURL}?key=${boxOfficeKey}&targetDt=20240925`);
     const boxOfficeList = boxOfficeResponse.data.boxOfficeResult.dailyBoxOfficeList;
-
+    console.log(movieList)
     // 매칭되지 않는 경우 추가 검색 처리
     const matchedMovies = await Promise.all(boxOfficeList.map(async (boxOfficeMovie) => {
       const matchedMovie = movieList.find(
@@ -54,9 +87,10 @@ export const fetchBoxOffice = async (movieList: MovieType[]) => {
 
       const poster = matchedMovie 
         ? matchedMovie.posters
-        : await searchMovieByName(boxOfficeMovie.movieNm);
+        : await searchMoviePoster(boxOfficeMovie.movieNm);
 
       return {
+        movieSeq: matchedMovie?.movieSeq || "",
         title: boxOfficeMovie.movieNm,
         posters: poster,
       };
