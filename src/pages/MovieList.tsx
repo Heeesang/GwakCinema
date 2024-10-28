e0cde1904218475e665bdeed98c0612783367800
Heesang <ghs050611@naver.com>
2024-10-29 02:53:29 +0900

    Update movieList gap

--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -10,7 +10,7 @@ export default function MovieList() {
         <div className="bg-black">
             <Header />
             <div className=" max-w-screen-2xl mx-auto px-40 pt-32">
-                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-y-8 gap-x-2">
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-y-8 gap-x-1">
                     {movieList?.slice(0, 15).map((movie, index) => (
                         <Link to={`/movie/${encodeURIComponent(movie.title)}/${movie.movieSeq}`}>
                             <Movie key={index} movie={movie} index={index} showIndex={false} />
