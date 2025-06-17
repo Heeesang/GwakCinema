975310585db8e68bbe1bcf0c289c7edb9e10b0f5
Heesang <ghs050611@naver.com>
2025-06-17 15:09:59 +0900

    Update page

--- src/hooks/useGetMovieList.ts
+++ src/hooks/useGetMovieList.ts
@@ -4,6 +4,6 @@ import { fetchMovies } from '../api/movieList';
 export const useGetMovieList = () =>
     useQuery({
         queryKey: ['fetchMovies'],
-        queryFn: () => fetchMovies(0),
+        queryFn: () => fetchMovies(1),
         staleTime: 24 * 60 * 60 * 1000
 });
